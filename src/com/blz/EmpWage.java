0d249eabb7a28158e3f3786bc4e6d3e235a9c160
pavithramuddu93 <pavithramuddu93>
2022-06-11 11:51:14 +0530

    UC5 added

--- src/com/blz/EmpWage.java
+++ src/com/blz/EmpWage.java
@@ -7,24 +7,26 @@ public class EmpWage {
     public static final int EMP_RATE_PER_HOUR = 20;
 
     public static int empHrs = 0;
+    public static int totalEmpHrs = 0;
+    public static int numOfWorkingDays = 20;
 
     private static void wageComputation() {
         Random random = new Random();
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                System.out.println("Employee is Present Full Time");
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee is Present Part Time ");
-                empHrs = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent ");
+        for (int i = 0; i < numOfWorkingDays; i++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+            }
+            totalEmpHrs = totalEmpHrs + empHrs;
         }
 
-        int empWage = empHrs * EMP_RATE_PER_HOUR;
+        int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage is : " + empWage);
     }
 
